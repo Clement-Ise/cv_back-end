018c9f911ff3b74f500faa2c42d15e78ebf48869
Clement  Iselin <clement-ise@MacBook-Pro-de-Clement.local>
2021-01-23 03:14:34 +0100

    create thrid class, class.Hobbie.php

---
+++ classes/class.Hobbie.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('class.Personne.php');
+
+class Hobbies{
+    private $id =0;
+    private $nom=null;
+    private $info =null;
+
+    private $laPersonne=array();
+
+}
+
+?>
---
+++ classes/class.Reseau.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('class.Personne.php');
+
+class Reseau{
+    private $id =0;
+    private $nom =null;
+    private $lien =null;
+    private $logo =null;
+
+    private $laPersonne = array();
+}
+
+
+?>
