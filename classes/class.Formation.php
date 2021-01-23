95e083d0e1d1aa13c3803623aeba174f62243e56
Clement  Iselin <clement-ise@MacBook-Pro-de-Clement.local>
2021-01-23 03:41:47 +0100

    save class.Formation.php and add associativedata, modifie attribut of private attribut on class.Personne.php

---
+++ classes/class.Formation.php
@@ -0,0 +1,14 @@
+<?php
+require_once('class.Personne.php');
+
+class Personne{
+    private $id =0;
+    private $nom =null;
+    private $etablissement = null;
+    private $ville =null;
+    private $departement =null;
+
+    private $laPersonne = null;
+
+}
+?>
--- classes/class.Personne.php
+++ classes/class.Personne.php
@@ -1,5 +1,11 @@
 <?php
 require_once('class.ExperiencePro.php');
+require_once('class.Reseau.php');
+require_once('class.Hobbie.php');
+require_once('class.Poste.php');
+require_once('class.Outil.php');
+require_once('class.Formation.php');
+
 
 class Personne{
     private $id = 0;
@@ -12,9 +18,15 @@ class Personne{
     private $adresse =null;
     private $mail =null;
     private $telephone =0;
-    private $permis = null;
-    private $voiture = null;
+    private $permis = false;
+    private $voiture = false;
 
-    private $lesExperiencePro = array();
+    private $lesExperiencesPros = array();
+    private $lesReseaux = array();
+    private $lesHobbies = array();
+    private $lesPostes = array();
+    private $lesOutils = array();
+    private $lesFormations = array();
+
 }
 ?>
